2e438a0948cdf4e8ae68aa4fd196ea738df936eb
Divy Gupta <divygupta208@gmail.com>
2024-07-26 14:09:28 +0530

    dynamic routing

--- models/product.js
+++ models/product.js
@@ -1,57 +1,48 @@
 const fs = require("fs");
 const path = require("path");
 
-const ensureDirectoryExistence = (filePath) => {
-  const dirname = path.dirname(filePath);
-  if (fs.existsSync(dirname)) {
-    return true;
-  }
-  ensureDirectoryExistence(dirname);
-  fs.mkdirSync(dirname);
-};
-
 const p = path.join(
-  path.dirname(require.main.filename),
+  path.dirname(process.mainModule.filename),
   "data",
   "products.json"
 );
 
 const getProductsFromFile = (cb) => {
-  fs.readFile(p, (err, data) => {
+  fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
-    }
-
-    try {
-      const products = JSON.parse(data);
-      cb(products);
-    } catch (parseErr) {
-      console.error("Error parsing JSON:", parseErr);
       cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
   });
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    ensureDirectoryExistence(p);
-
+    this.id = Math.random().toString();
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
-        if (err) {
-          console.error("Error writing file:", err);
-        }
+        console.log(err);
       });
     });
   }
 
   static fetchAll(cb) {
-    ensureDirectoryExistence(p);
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
   }
 };
