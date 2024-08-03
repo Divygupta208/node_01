ca35b38c21328f8b2561b1de39974bfab55a7c70
Divy Gupta <divygupta208@gmail.com>
2024-08-04 01:33:25 +0530

    connecting to sql database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const db = require("./util/database");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -10,6 +10,14 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
+db.execute("SELECT * FROM products")
+  .then((result) => {
+    console.log(result[0]);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
